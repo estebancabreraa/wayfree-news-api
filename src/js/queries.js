d63bda34f9f4b2029f202ede5908257e48906f0d
estebancabreraa <esteban.cabrera.a@gmail.com>
2022-06-06 16:51:47 -0600

    queries added && improvements

--- src/js/queries.js
+++ src/js/queries.js
@@ -20,8 +20,7 @@ const getUserCredentials = (request, response) => {
     
     //If query found more than one result with email, password parameters...
     if (results.rows.length > 0){ 
-     
-      response.status(200).json({status: true})
+      response.status(200).json({user_id: results.rows[0].user_id, user_name: results.rows[0].user_name, role: results.rows[0].user_role, status: true})
         
     } else {response.status(200).json({status: false})}
   })
@@ -35,6 +34,28 @@ const getArticles = (request, response) => {
     console.log("Sending articles to client...")
     response.status(200).json(results.rows[0])
     console.log("Done.")
+  })
+}
+
+const getLastArticleID = (request, response) => {
+  pool.query(process.env.GET_LAST_ARTICLE_ID_QUERY, (error, results) => {
+    if (error) {
+      throw error
+    }
+    console.log("Sending article id to client...")
+    response.status(200).json(results.rows[0])
+    console.log("Done.")
+  })
+}
+
+
+
+const getContentID = (request, response) => {
+  pool.query(process.env.GET_CONTENT_ID_QUERY, (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
   })
 }
 
@@ -52,11 +73,22 @@ const getCategories = (request, response) => {
     if (error) {
       throw error
     }
-    console.log("Sending articles to client...")
-    response.status(200).json(results.rows[0])
+    console.log("Sending article categories to client...")
+    response.status(200).json(results.rows)
+    console.log(results.rows)
     console.log("Done.")
   })
 }
+
+const getUserID = (request, response) => {
+  pool.query(process.env.GET_USER_ID_QUERY, [user_name], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
+
 
 const insertArticleContent = (request, response) => {
   const {file_name} = request.body
@@ -81,7 +113,7 @@ const insertImageSet = (request, response) => {
 }
 
 const insertArticle = (request, response) => {
-  const {title, date, category, content, image_set, author} = request.body
+  const {title, date, category, author} = request.body
 
   pool.query(process.env.INSERT_ARTICLE_QUERY, [title, date, category, content, image_set, author], (error, results) => {
     if (error) {
@@ -105,6 +137,7 @@ const updateArticle = (request, response) => {
 module.exports = {
   getUserCredentials,
   getArticles,
+  getLastArticleID,
   getAction,
   getCategories,
   insertArticleContent,
